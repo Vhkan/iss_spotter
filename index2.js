1d97cd08ba700423b8b0e65b48df2287eafee5f8
Vhkan <vhkan@yahoo.com>
2023-10-03 06:52:32 -0600

    Adding iss_promised.js and index2.js files and refactoring code

---
+++ index2.js
@@ -0,0 +1,17 @@
+// const { fetchCoordsByIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require ('./iss_promised');
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+
+fetchMyIP()
+.then(fetchCoordsByIP)
+.then(fetchISSFlyOverTimes)
+.then(body => console.log(body));
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
